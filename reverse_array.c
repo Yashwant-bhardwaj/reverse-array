1544c557333e3d1bfab08d06c6ec8c783d5129f1
Yashwant-bhardwaj <yashwantbhardwaj7988810@gmail.com>
2025-02-14 10:36:49 +0530

    Creat_reverse_array.c
    This is the basic approach for solving the quetions

---
+++ reverse_array.c
@@ -0,0 +1,22 @@
+// reverse the array                  14 feb 
+#include<stdio.h>
+int main()
+{
+    int a[5]={10,20,30,40,50};
+    // creating a new array 
+    int ab[5];
+    for(int i=0;i<5;i++)
+    {
+        ab[i]=a[4-i];
+    }
+    printf("print the all elements of the 1st array\n");
+    for(int i=0;i<5;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    printf("print all elements of the 2nd array\n") ;
+    for(int i=0;i<5;i++)
+    {
+        printf("%d\n",ab[i]);
+    }
+}
